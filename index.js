e8f5acfe536bf3242b4eeb55e916c4572bbb1193
Maria Marquez <406rem33@gmail.com>
2022-04-04 19:45:21 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+function superbowlWin(record){
+    function didWin(year){
+        if (year.result === "W"){
+            return true;
+        }
+        else {return false;}
+    }
+const winningYear = record.find(didWin);
+if (winningYear){
+    return winningYear.year;
+}
+}   
+
+
